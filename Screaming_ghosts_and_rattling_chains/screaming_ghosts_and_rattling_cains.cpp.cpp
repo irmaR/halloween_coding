f0b97777712f4b2015135ec01353df99513c6818
Irma <irma.ravkic@cs.kuleuven.be>
2018-11-11 13:27:37 -0800

    first commit

---
+++ Screaming_ghosts_and_rattling_chains/screaming_ghosts_and_rattling_cains.cpp.cpp
@@ -0,0 +1,15 @@
+//Streaming ghosts and rattling chains in C/C++
+#include <stdio.h> 
+int main() 
+{ 
+  int a = 10, b = 20, c = 30; 
+  if (c > b > a)   // :O
+   printf("TRUE"); // :O
+  else
+    printf("FALSE"); // :O
+  return 0; 
+  //Question 1: what should be the output?
+  //Question 2: explain why the output happened?
+  //Question 3: use parantheses to describe how the c>b>a expression evaluated
+  
+}
